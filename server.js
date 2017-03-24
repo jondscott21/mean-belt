61afdc9f38d5731abaca22c401e221ea0c1a2f0b
Jonathan Scott <jonathan.scott@gmail.com>
2017-03-24 00:17:31 -0700

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express  = require( 'express' );
 const bp = require('body-parser');
 const path = require( 'path' );
-const port = process.env.PORT || 7777,
+const port = process.env.PORT || 8001,
     app = express();
 
 //=====Body-Parser=====//
